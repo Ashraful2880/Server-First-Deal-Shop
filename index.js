50786f734c323a01642b300b9d30bc1c557607a2
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-10 01:34:03 +0600

    SSL Commerce Initialize And Success Route added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const SSLCommerzPayment = require("sslcommerz");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //<------------- Database Code Here ---------->
 
@@ -84,6 +87,77 @@ async function run() {
     app.get("/about", async (req, res) => {
       const ourTeams = await teams.find({}).toArray();
       res.send(ourTeams);
+    });
+
+    app.post("/init", async (req, res) => {
+      const productInfo = {
+        total_amount: req.body.total_amount,
+        currency: "USD",
+        tran_id: uuidv4(),
+        success_url: "http://localhost:5000/success",
+        fail_url: "http://localhost:5000/fail",
+        cancel_url: "http://localhost:5000/cancel",
+        ipn_url: "http://localhost:5000/ipn",
+        shipping_method: "Courier",
+        product_name: req.body.product_name,
+        product_category: "Electronic",
+        product_profile: req.body.product_profile,
+        product_image: req.body.product_image,
+        cus_name: req.body.cus_name,
+        cus_email: req.body.cus_email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: req.body.cus_name,
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+        multi_card_name: "mastercard",
+        value_a: "ref001_A",
+        value_b: "ref002_B",
+        value_c: "ref003_C",
+        value_d: "ref004_D",
+      };
+      console.log(productInfo);
+      const result = await OrderCollections.insertOne(productInfo);
+      const sslcommer = new SSLCommerzPayment(
+        process.env.STORE_ID,
+        process.env.STORE_PASSWORD,
+        false
+      ); //true for live default false for sandbox
+      sslcommer.init(productInfo).then((data) => {
+        res.json(data.GatewayPageURL);
+
+        const orderInfo = { ...productInfo, ...data };
+        if (orderInfo.GatewayPageURL) {
+          res.json(orderInfo.GatewayPageURL);
+        } else {
+          return res.status(400).json({
+            message: "SSL session was not successful",
+          });
+        }
+      });
+    });
+
+    app.post("/success", async (req, res) => {
+      console.log(req.body);
+      res.status(200).json(req.body);
+    });
+    app.post("/fail", async (req, res) => {
+      console.log(req.body);
+      res.status(400).json(req.body);
+    });
+    app.post("/cancel", async (req, res) => {
+      console.log(req.body);
+      res.status(200).json(req.body);
     });
 
     //<------------ Get Payment From User And Send to DB ------------->
