4e379f6d52d113023399000ced1eec3325b7a239
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-10 20:25:29 +0600

    All API Test Success

--- index.js
+++ index.js
@@ -160,12 +160,14 @@ async function run() {
       );
       res.redirect(`http://localhost:3000/success/${req.body.tran_id}`);
     });
+
     app.post("/failure", async (req, res) => {
       const order = await OrderCollections.deleteOne({
         tran_id: req.body.tran_id,
       });
       res.redirect(`http://localhost:3000/fail`);
     });
+
     app.post("/cancel", async (req, res) => {
       const order = await OrderCollections.deleteOne({
         tran_id: req.body.tran_id,
